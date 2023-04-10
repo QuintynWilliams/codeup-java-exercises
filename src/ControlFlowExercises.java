d95795eac48435c332bce29abcc20a28ebfdc7f6
Quintyn Williams <t.quin.williams@gmail.com>
2023-04-10 15:11:37 -0500

    Display table of multiples

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class ControlFlowExercises {
     public static void main(String[] args) {
 
@@ -42,7 +44,32 @@ public class ControlFlowExercises {
 //        }
 
 
+//      2. Fizzbuzz
+//        for (int i = 1; i <= 100; i++) {
+//            if (i % 3 == 0 && i % 5 == 0) {
+//                System.out.println("FizzBuzz");
+//            } else if (i % 3 == 0) {
+//                System.out.println("Fizz");
+//            } else if (i % 5 == 0) {
+//                System.out.println("Buzz");
+//            }  else {
+//                System.out.println(i);
+//            }
+//        }
 
+//      3. Display a table of powers.
+        Scanner sc = new Scanner(System.in);
+        System.out.println("What number would you like to go up to?");
+        int userInput = sc.nextInt();
+
+        System.out.println("number | squared | cubed");
+        System.out.println("------ | ------- | -----");
+
+        for (int i = 1; i <= userInput; i++) {
+            int square = i * i;
+            int cube = i * i * i;
+            System.out.printf("%-6s | %-7s | %-6s \n", i, square, cube);
+        }
 
     }
 }
