a5b655b827d83eb4be5bb6103e4b92af462ffc82
Quintyn Williams <t.quin.williams@gmail.com>
2023-04-11 11:55:28 -0500

    bob responsiveness

--- src/Bob.java
+++ src/Bob.java
@@ -3,17 +3,28 @@ import java.util.Scanner;
 public class Bob {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("Would you like to talk to Bob? [Y/N]");
+        String getInput = sc.nextLine();
+        boolean talk = getInput.equalsIgnoreCase("Y");
 
-        String userInput = sc.nextLine();
+        while (talk == true) {
+            System.out.println("What would you like to say to Bob?");
+            String userInput = sc.nextLine();
 
-        if (userInput.endsWith("?")) {
-            System.out.println("Sure");
-        } else if (userInput.endsWith("!")) {
-            System.out.println("Whoa, chill out!");
-        } else if (userInput.equals("")) {
-            System.out.println("Fine. Be that way!");
-        } else {
-            System.out.println("Whatever");
+            if (userInput.endsWith("?")) {
+                System.out.println("Bob: Sure");
+            } else if (userInput.endsWith("!")) {
+                System.out.println("Bob: Whoa, chill out!");
+            } else if (userInput.isBlank()) {
+                System.out.println("Bob: Fine. Be that way!");
+            } else {
+                System.out.println("Bob: Whatever");
+            }
+
+            System.out.println("Would you like to talk to Bob? [Y/N]");
+            getInput = sc.nextLine();
+            talk = getInput.equalsIgnoreCase("Y");
         }
+
     }
 }
