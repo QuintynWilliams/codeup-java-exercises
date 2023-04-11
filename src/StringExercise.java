9f3829bed15f7f1f98ee5886166fe19bfed4d13a
Quintyn Williams <t.quin.williams@gmail.com>
2023-04-11 09:25:57 -0500

    first exercise

--- src/StringExercise.java
+++ src/StringExercise.java
@@ -1,3 +1,15 @@
 public class StringExercise {
+    public static void main(String[] args) {
 
+        String ctrl = "";
+
+        if (ctrl.startsWith("")) {
+            ctrl += "We don't need no education\n";
+        }
+        if (ctrl.endsWith("education\n")) {
+            ctrl += "We don't need no thought control";
+        }
+
+        System.out.println(ctrl);
+    }
 }
