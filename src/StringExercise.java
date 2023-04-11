85a12ebfd115c684ad61e5c4c530b323e51b08ea
Quintyn Williams <t.quin.williams@gmail.com>
2023-04-11 09:33:25 -0500

    String escapes

--- src/StringExercise.java
+++ src/StringExercise.java
@@ -1,15 +1,19 @@
 public class StringExercise {
     public static void main(String[] args) {
 
-        String ctrl = "";
-
-        if (ctrl.startsWith("")) {
-            ctrl += "We don't need no education\n";
-        }
-        if (ctrl.endsWith("education\n")) {
-            ctrl += "We don't need no thought control";
-        }
-
+        String ctrl =   "We don't need no education\n" +
+                        "We don't need no thought control\n";
         System.out.println(ctrl);
+
+        String mark =   "Check \"this\" out!, \"s inside of \"s\n";
+        System.out.println(mark);
+
+        String cmd =    "In windows, the main drive is usually C:\\\n";
+        System.out.println(cmd);
+
+        String back =   "I can do backslashes \\, double backslashes \\\\,\n" +
+                        "and the amazing triple backslash \\\\\\!\n";
+        System.out.println(back);
+
     }
 }
