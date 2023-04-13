409a94292d18e06074d20b9421d240f93ec31034
Quintyn Williams <t.quin.williams@gmail.com>
2023-04-13 14:50:17 -0500

    file create, structure, and import

---
+++ src/movies/Movie.java
@@ -0,0 +1,14 @@
+package movies;
+
+public class Movie {
+
+    private String name;
+    private String category;
+
+//    TODO: Constructor that sets both of these.
+//     Create methods to access these properties and change them (getters
+//     and setters).
+
+
+
+}
---
+++ src/movies/MoviesApplication.java
@@ -0,0 +1,13 @@
+package movies;
+
+import util.Input;
+
+public class MoviesApplication {
+
+    public static void main(String[] args) {
+
+
+
+    }
+
+}
