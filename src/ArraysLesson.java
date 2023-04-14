b35ce3667cb75e52cbd3b09ca677afa50c1898b7
Quintyn Williams <t.quin.williams@gmail.com>
2023-04-14 09:22:30 -0500

    Creating and Looping through Java Arrays

--- src/ArraysLesson.java
+++ src/ArraysLesson.java
@@ -2,6 +2,7 @@ public class ArraysLesson {
 
     public static void main(String[] args) {
 
+//      Use for variable driven arrays or manipulating the array
         int sizeofArray = 5;
 
         int[] numbers = new int[sizeofArray];
@@ -13,9 +14,22 @@ public class ArraysLesson {
         numbers[4] = 1267;
 
 //  Array Initializer Syntax
-
+//      Use when you know what is being put in the array
         int[] moreNumbers = {45, 56, 67, 78};
 
+        System.out.println("Array Initializer: " + moreNumbers[2]);
+        System.out.println("Array: " + numbers[2]);
+
+//  Array Loops
+//      Enhanced for loop when you dont need the INDEX
+        for (int number : numbers) {
+            System.out.printf("Num at index: %d%n", number);
+        }
+
+//      Trad For Loop
+        for (int i = 0; i < moreNumbers.length; i++) {
+            System.out.printf("Num at index %d: %d%n", i, moreNumbers[i]);
+        }
 
     }
 
