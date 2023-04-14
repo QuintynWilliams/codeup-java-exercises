4b2680d50852314cdad6f2010e19e920a526ecda
Quintyn Williams <t.quin.williams@gmail.com>
2023-04-14 10:04:21 -0500

    creat objects array and print

--- src/ArraysLesson.java
+++ src/ArraysLesson.java
@@ -1,6 +1,13 @@
+import OOP_notes_by_rpg.Monster;
+
+import java.util.Arrays;
+
 public class ArraysLesson {
+//    v - - Must be declared static in order to be declared outside of method
+//    static Monster school = new Monster("school", 24, 234, 24);
 
     public static void main(String[] args) {
+//        System.out.println(school);
 
 //      Use for variable driven arrays or manipulating the array
         int sizeofArray = 5;      // V - - - - - Can be declared HERE
@@ -14,22 +21,36 @@ public class ArraysLesson {
 //      Use when you know what is being put in the array
         int[] moreNumbers = {45, 56, 67, 78};
 
-
-        System.out.printf("Array: %d",numbers[2]);
+//        System.out.printf("Array: %d",numbers[2]);
 //  Array Loops
 //      Enhanced for loop when you dont need the INDEX
-        for (int number : numbers) {
-            System.out.printf("Num at index: %d%n", number);
-//          numbers[3] = 0, as a DEFAULT VALUE (was not declared)
-        }
+//        for (int number : numbers) {
+//            System.out.printf("Num at index: %d%n", number);
+////          numbers[3] = 0, as a DEFAULT VALUE (was not declared)
+//        }
 
-        System.out.printf("%nArray Initializer: %d",moreNumbers[2]);
+//        System.out.printf("%nArray Initializer: %d",moreNumbers[2]);
 //      Trad For Loop
-        for (int i = 0; i < moreNumbers.length; i++) {
-            System.out.printf("Num at index %d: %d%n", i, moreNumbers[i]);
-        }
+//        for (int i = 0; i < moreNumbers.length; i++) {
+//            System.out.printf("Num at index %d: %d%n", i, moreNumbers[i]);
+//        }
 
-//      ARRAYS of OBJECTS: SEE src/OOP_notes_by_rpg
+//      ARRAYS of OBJECTS: SEE src/OOP_notes_by_rpg/Monster
+//        Creating objects based on Traits of the Monster Class
+//        Uses Constructor to set values
+        Monster orc = new Monster("Orc", 13, 15, 9);
+        Monster ogre = new Monster("Ogre", 11, 59, 13);
+        Monster blueDragon = new Monster("Blue Dragon", 19, 225, 23);
+
+        Monster[] monsters = {orc, ogre, blueDragon};
+
+        for (Monster monster : monsters) {
+            System.out.printf("""
+                   %s \s
+                   Armor |  HP  |  DMG  \s 
+                    %d      %d     %d   %n
+                    """, monster.getName(), monster.getArmorClass(), monster.getHitPoints(), monster.getDamage());
+        }
 
     }
 
