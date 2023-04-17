9cbd0c75696b95aca2561c0014638d58c949cb5a
Quintyn Williams <t.quin.williams@gmail.com>
2023-04-17 09:40:02 -0500

    subclasses and organic methods

--- src/birds/Bird.java
+++ src/birds/Bird.java
@@ -12,8 +12,32 @@ public class Bird {
         this.name = name;
     }
 
+//  Generic Method
     public void makeNoise ()  {
-        System.out.println("*tweet, tweet*");
+        System.out.printf("%s: *tweet, tweet*%n", this.name);
+    }
+
+    public void layEgg(){
+        System.out.printf("%s: *lays egg*%n", this.name);
+    }
+
+}
+
+class Quaker extends Bird {
+    public String name;
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void makeNoise () {
+        System.out.printf("%s: *ca-caw*%n", this.name);
     }
 
 }
--- src/birds/BirdTest.java
+++ src/birds/BirdTest.java
@@ -4,10 +4,16 @@ public class BirdTest {
 
     public static void main(String[] args) {
 
-        //    Since no constructor exists, name cant go inside paranthesis
-        Bird quaker = new Bird();
+//      Since no constructor exists, name cant go inside paranthesis
+        Quaker quaker = new Quaker();
         quaker.setName("Quaker");
+
+//      Uses Subclass makeNoise function IOT:
+//      QuakerClass: CAW rather than BirdClass: Tweet
         quaker.makeNoise();
+
+//      Still may utilize superclass (BirdClass) method
+        quaker.layEgg();
 
     }
 
