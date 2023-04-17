9a4d2004f7a31670273eb8a4f369a27ec1ab415e
Quintyn Williams <t.quin.williams@gmail.com>
2023-04-17 09:20:10 -0500

    initial code

--- src/birds/Bird.java
+++ src/birds/Bird.java
@@ -2,5 +2,18 @@ package birds;
 
 public class Bird {
 
+    private String name;
+
+//  GET && SET
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void makeNoise ()  {
+        System.out.println("*tweet, tweet*");
+    }
 
 }
---
+++ src/birds/BirdTest.java
@@ -0,0 +1,14 @@
+package birds;
+
+public class BirdTest {
+
+    public static void main(String[] args) {
+
+        //    Since no constructor exists, name cant go inside paranthesis
+        Bird quaker = new Bird();
+        quaker.setName("Quaker");
+        quaker.makeNoise();
+
+    }
+
+}
