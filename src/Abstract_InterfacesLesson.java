1a0cf5e24c0f9e112f538e294500567f64c0ab2c
Quintyn Williams <t.quin.williams@gmail.com>
2023-04-18 11:00:52 -0500

    implementation

--- src/Abstract_InterfacesLesson.java
+++ src/Abstract_InterfacesLesson.java
@@ -6,6 +6,14 @@ public class Abstract_InterfacesLesson {
         Developer newGuy = new Developer("Frank", "Front-end");
         System.out.println(newGuy.work());
 
+        Bear bigBear = new Bear("Yogi", "Grizzly", 600.00, true);
+        System.out.println(bigBear.skin());
+
+        String[] weddingItems = {"Baby Carriage", "Toaster", "Fondue Kit", "Single China Set"};
+        WishList newWishList = new WishList("Wedding Registry", weddingItems);
+        String newShareLink = newWishList.shareLink();
+        System.out.println(newWishList.getName() + " => " + newShareLink);
+
     }
 
 }
@@ -53,11 +61,119 @@ class Developer extends Employee {
     }
 }
 
-abstract class Notification {
 
-    public abstract boolean didOptIn();
-    public abstract String unsubscribeLink();
-    public abstract String getPhysicalAddress();
-    public abstract String getSenderInfo();
+abstract class Animal {
+    //  Fields
+    protected String name;
+    protected String species;
+    protected Double weight;
+    protected boolean islegendary;
+//  GET/SET
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getSpecies() {
+        return species;
+    }
+    public void setSpecies(String species) {
+        this.species = species;
+    }
+    public Double getWeight() {
+        return weight;
+    }
+    public void setWeight(Double weight) {
+        this.weight = weight;
+    }
+    public boolean isIslegendary() {
+        return islegendary;
+    }
+    public void setIslegendary(boolean islegendary) {
+        this.islegendary = islegendary;
+    }
+//  Constructor
+    public Animal() {}
+    public Animal(String name, String species, Double weight, boolean islegendary) {
+        this.name = name;
+        this.species = species;
+        this.weight = weight;
+        this.islegendary = islegendary;
+    }
 
 }
+
+// Applies Skinnable method(s) to the SUBc Bear
+// Animal !| Bear has access to these methods w/o implementation
+// Protects the classes from running methods the user/program intends to
+class Bear extends Animal implements Skinnable {
+
+//  Methods
+    @Override
+    public String skin() {
+        if (this.islegendary) {
+            return "Legendary Bear Hide";
+        }
+        return "Bear Hide";
+    }
+
+//  Constructor
+    public Bear(String name, String species, Double weight, boolean islegendary) {
+        super(name, species, weight, islegendary);
+    }
+
+}
+
+
+abstract class List {
+
+    protected String name;
+
+    protected String[] items;
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    public String[] getItems() {
+        return items;
+    }
+    public void setItems(String[] items) {
+        this.items = items;
+    }
+
+    public List() {}
+    public List(String name, String[] items) {
+        this.name = name;
+        this.items = items;
+    }
+}
+
+class WishList extends List implements Shareable, Deletable {
+
+    public WishList(String name, String[] items) {
+        super(name, items);
+    }
+
+    @Override
+    public String shareLink() {
+        return "https://mysharelink.com";
+    }
+
+    @Override
+    public String delete() {
+        return this.name + " deleted";
+    }
+}
+
+class PrivateList extends List {
+    public PrivateList(String name, String[] items) {
+        super(name, items);
+    }
+}
+
