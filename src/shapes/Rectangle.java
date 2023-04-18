0d67d258b415e0e1a7fc665ab67685eb5e0f52b0
Quintyn Williams <t.quin.williams@gmail.com>
2023-04-18 11:47:02 -0500

    rmv extra lines

--- src/shapes/Rectangle.java
+++ src/shapes/Rectangle.java
@@ -2,20 +2,11 @@ package shapes;
 
 public class Rectangle extends Quadrilateral implements Measurable {
 
-    protected double length;
-    protected double width;
-
     public void setDimensions(double length, double width) {
         this.length = length;
         this.width = width;
     }
 
-    public double getWidth() {
-        return width;
-    }
-    public double getLength() {
-        return length;
-    }
 
     @Override
     public double getPerimeter() {
